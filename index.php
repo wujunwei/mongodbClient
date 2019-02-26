2146babc3fcc62d8702e23ae62ad02fab43c4734
adam <adam.wu@shein.com>
2019-02-26 10:50:48 +0800

    add test case

--- index.php
+++ index.php
@@ -9,13 +9,26 @@
 
 require "./vendor/autoload.php";
 
-$test = new \Test\TestCase("5a7a980dbf2afb1e7c003aa1");
+$config = [
+    'connect_str' => 'mongodb://127.0.0.1:27017/test',
+    'dbname'=>'test',
+    'option' => [
+//        'username'=>'configadmin',
+//        'password'=>'uU3yTmb2c',
+//        'heartbeatFrequencyMS'=>2000,
+//        'replicaSet'=>'shein',
+//        'readPreference'=>'primaryPreferred',
+    ],
+];
+
+
+$test = new \Test\TestCase();
 $test->bsonUnserialize(['rew' => 3212]);
-$client = new \Shein\MongoDBClient('mongodb://127.0.0.1:27017/', 'test', ['heartbeatFrequencyMS'=>2000, 'readPreference'=>'primaryPreferred',], ['typeMap' => ['root' => \Shein\Entity::class]]);
-$em = new \Shein\EntityManage($client);
+$client = new \Shein\MongoDBClient('mongodb://127.0.0.1:27017/', 'test', ['heartbeatFrequencyMS'=>2000, 'readPreference'=>'primaryPreferred',], ['typeMap' => ['root' => \Test\TestCase::class]]);
+$em = new \Shein\EntityManage($config);
 var_dump($em->merge($test));
-//$client->getCollection('test')->insertOne($test);
-//print_r($client->getCollection('test')->findAll());
+$client->getCollection('test')->insertOne($test);
+print_r($client->getCollection('test')->findAll());
 
 //foreach ($client->listCollection() as $collection){
 //    var_dump($collection);
